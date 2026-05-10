96251126ee0e5f00dec8d394a04ad3fe479b8592
Raina <cgl0326@outlook.com>
2026-05-10 03:11:24 +0100

    0510

--- armstrong-numbers/src/lib.rs
+++ armstrong-numbers/src/lib.rs
@@ -1,3 +1,4 @@
 pub fn is_armstrong_number(num: u32) -> bool {
     let number_of_digit = if num == 0 { 1 } else { num.ilog10() + 1 };
+    num == num.to_string().chars().map(|char| char.to_digit(10).unwrap().pow(number_of_digit)).sum()
 }
